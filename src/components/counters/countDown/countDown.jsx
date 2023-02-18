998151f9ceb6dbddd7a3ce17618ab42bf384d7c7
MrfroxiW <froxi340@mail.ru>
2023-02-18 10:51:53 +0300

    feat:create components

---
+++ src/components/counters/countDown/countDown.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef, useState } from 'react'
+import ElemCounterTimer from '../../../entities/elemCounterTimer/elemCounterTimer';
+import classes from './countDown.module.css'
+
+const CountDown =  () => {
+    let [seconds,setSecond] = useState('00')
+    let [minets,setMinet] = useState('00')
+    let [hours,setHour] = useState('00')
+    let [days,setDay] = useState('00')
+
+    // eslint-disable-next-line no-unused-vars
+    let interval = useRef();
+
+    const startTimer= ( ) => {
+  
+            const countDownDate = new Date('May 31 2023 00:00:00').getTime();
+            interval = setInterval( () => {
+
+            const timeNow = new Date().getTime();
+            const distance = countDownDate -timeNow ;
+
+            const notUtcDays = Math.floor(distance/(1000*60*60*24))
+            const notUtcHours = Math.floor(distance%(1000*60*60*24) / ( 1000 * 60 *60))
+            const notUtcMinutes = Math.floor(distance%(1000*60*60) / (1000*60))
+            const notUtcSeconds = Math.floor(distance%(1000*60)/1000 )
+
+            if(distance >=0) {
+                setSecond(notUtcSeconds);
+                setMinet(notUtcMinutes);
+                setHour(notUtcHours);
+                setDay(notUtcDays);
+            }
+
+        },1000)     
+    }
+
+    useEffect(() => {
+        startTimer();
+    })
+
+ return (
+   <div className={classes.countDown}>
+    <ElemCounterTimer num={seconds} type={'seconds'}/>
+    <ElemCounterTimer num={minets} type={'minets'}/>
+    <ElemCounterTimer num={hours} type={'hours'}/>
+    <ElemCounterTimer num={days} type={'days'}/>
+   </div>
+ );
+};
+
+export default CountDown;
